f3981c93a3442b96bec47b49c8d4f3bb1cceaac6
thejavidev <javidation.l@gmail.com>
2023-06-17 00:22:19 +0400

    34

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,10 +6,13 @@ import "swiper/css/effect-fade";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { EffectFade, Autoplay, Pagination } from "swiper";
+import { AiOutlineRight } from 'react-icons/ai';
+
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { Link } from 'react-router-dom';
 
 const Home = ({ data }) => {
   const homebanner = data?.homebanner;
@@ -50,23 +53,29 @@ const Home = ({ data }) => {
         </Swiper>
         <section id="2">
           <Swiper
-            spaceBetween={10}
+            spaceBetween={0}
             slidesPerView={4}
-            autoplay={{
-              delay: 4500,
-              disableOnInteraction: false,
-            }}
+
             className="mySwiper"
           >
             {
               services && services?.map((cur, i) => (
-                <SwiperSlide key={i} className='services '>
-                 
-                  <div className="flex justify-between items-center flex-col h-full w-full">
-                    <div className="">
-                      <LazyLoadImage c src={cur?.cover} className='w-full h-full current' />
+                <SwiperSlide key={i} className='services p-[30px] h-[280px] relative'>
+                  <Link to={`/services/${cur?.slug}`}>
+                    <div className="flex justify-between items-center flex-col h-full w-full">
+                      <div className="">
+                        <LazyLoadImage src={cur?.cover} className='w-full h-[70px] current' />
+                        <LazyLoadImage src={cur?.cover_hover} className='w-full h-[70px] current onhover hidden' />
+                      </div>
+                      <div className='font-[700] text-[16px] uppercase text-[#fff] pt-[20px] mb-[10px] pl-[0] pr-[0]' dangerouslySetInnerHTML={{ __html: cur?.name_az }}></div>
+                      <span className='bg-[#4A9CC2] w-[80px] h-[2px] block ' />
+                      <p className='font-[200] text-[14px] text-center pb-[20px] text-[#fff]' dangerouslySetInnerHTML={{ __html: cur?.text_az }}></p>
+                      <Link to={`/services/${cur?.slug}`} className='border-1 p-[5px] rounded-full'>
+
+                        <AiOutlineRight className='text-[#fff]  ' />
+                      </Link>
                     </div>
-                  </div>
+                  </Link>
                 </SwiperSlide>
               ))
             }
